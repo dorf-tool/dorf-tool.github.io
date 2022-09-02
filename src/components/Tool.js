2d08ad3461663ee36e468949bbce1bb61bab892a
Fazhe <bfazhe@gmail.com>
2022-09-02 16:46:59 +0800

    feat: 添加jotai

--- src/components/Tool.js
+++ src/components/Tool.js
@@ -6,31 +6,56 @@ import PieceCard from "./PieceCard";
 import PieceInput from "./PieceInput";
 import Button from "./Button";
 import { TYPE_REG_STRS } from "../constants";
-const Tool = () => {
-  const [pieces, setPieces] = useState(
-    localStorage.getItem("pieces")
-      ? JSON.parse(localStorage.getItem("pieces"))
-      : []
+
+import { useAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+
+// 需求地块
+const requirePiecesAtom = atomWithStorage("pieces", []);
+// 待匹配地块
+const matchPieceAtom = atomWithStorage("matchPieceAtom", "");
+
+const Title = ({ children, ...props }) => {
+  return (
+    <div
+      className="text-lg font-sans font-semibold text-blue-grey-800 p-1"
+      {...props}
+    >
+      {children}
+    </div>
   );
-  const [piece, setPiece] = useState("");
+};
+
+const Tool = () => {
+  const [requirePieces, setRequirePieces] = useAtom(requirePiecesAtom);
+  const [matchPiece, setMatchPiece] = useAtom(matchPieceAtom);
+
+  // 输入后添加到需求地块
   const addPiece = (piece) => {
-    setPieces([...pieces, piece]);
+    setRequirePieces([...requirePieces, piece]);
   };
+
+  // 从需求地块内删除
   const removePiece = (piece) => {
-    const index = pieces.indexOf(piece);
-    setPieces(pieces.filter((_, i) => i !== index));
+    const index = requirePieces.indexOf(piece);
+    setRequirePieces(requirePieces.filter((_, i) => i !== index));
   };
+  // 清空需求地块
   const clearPieces = () => {
     // confirm确认是否清空pieces
     const r = window.confirm("是否清空地块");
     if (r) {
-      setPieces([]);
+      setRequirePieces([]);
     }
   };
+
+  // 将待匹配地块移动到需求地块
   const moveToPieces = () => {
-    setPieces([...pieces, piece]);
-    setPiece("");
+    setRequirePieces([...requirePieces, matchPiece]);
+    setMatchPiece("");
   };
+
+  // 对比地块
   const comparePiece = (a, b) => {
     // a循环拼接翻倍
     const aStr = a + a;
@@ -42,34 +67,29 @@ const Tool = () => {
     const result = aStr.match(bReg);
     return result && result.length > 0;
   };
-  useEffect(() => {
-    // 缓存pieces
-    localStorage.setItem("pieces", JSON.stringify(pieces));
-  }, [pieces]);
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-2 absolute inset-0 h-screen w-screen p-2 nm-flat-blue-grey-100 ">
+    <div className="grid grid-cols-1 md:grid-cols-2 md:gap-2 absolute inset-0 h-screen w-screen p-2 nm-flat-blue-grey-100 ">
       {/* 左 */}
-      <div className="col-span-1 flex flex-col  overflow-auto md:overflow-hidden rounded-lg p-2 ">
-        <p className="text-lg font-sans font-semibold text-blue-grey-800">
-          输入当前需求地块
-        </p>
+      <div className="col-span-1 flex flex-col overflow-auto md:overflow-hidden rounded-lg p-2">
+        <Title>输入当前需求地块</Title>
         <PieceInput onFinish={addPiece} />
-        <p className="text-lg font-sans font-semibold text-blue-grey-800">
-          需求地块
-        </p>
-        <div className="flex-auto w-full flex flex-wrap content-start justify-center md:overflow-auto nm-flat-blue-grey-100 p-2 rounded-md relative">
-          {pieces.length > 0 && (
+        <Title>需求地块</Title>
+        <div className="flex-auto w-full flex flex-wrap content-start justify-center md:overflow-auto nm-flat-blue-grey-100 p-2 rounded-md relative gap-3">
+          {requirePieces.length > 0 && (
             <FontAwesomeIcon
               className="absolute top-1 right-1 text-red-400 cursor-pointer text-lg z-10"
               onClick={clearPieces}
               icon={faTrashCan}
             />
           )}
-          {pieces.map((p, index) => (
+          {requirePieces.map((p, index) => (
             <PieceCard
               matched={
-                piece &&
-                pieces.filter((p) => comparePiece(p, piece)).includes(p)
+                matchPiece &&
+                requirePieces
+                  .filter((p) => comparePiece(p, matchPiece))
+                  .includes(p)
               }
               key={p + index}
               piece={p}
@@ -79,25 +99,23 @@ const Tool = () => {
         </div>
       </div>
       {/* 右 */}
-      <div className="col-span-1 flex flex-col overflow-auto md:overflow-hidden rounded-lg  p-2">
-        <p className="text-lg font-sans font-semibold text-blue-grey-800">
-          输入匹配地块
-        </p>
-        <PieceInput onFinish={setPiece} />
-        <p className="text-lg font-sans font-semibold text-blue-grey-800">
-          匹配地块
-        </p>
+      <div className="col-span-1 flex flex-col overflow-auto md:overflow-hidden rounded-lg p-2">
+        <Title>输入匹配地块</Title>
+        <PieceInput onFinish={setMatchPiece} />
+        <Title>匹配地块</Title>
         <div className="h-36 nm-flat-blue-grey-100 p-2 rounded-md flex  items-center">
-          <PieceCard piece={piece} />
-          {piece && <Button onClick={moveToPieces}>添加到需求</Button>}
+          {matchPiece && (
+            <>
+              <PieceCard piece={matchPiece} />
+              <Button onClick={moveToPieces}>添加到需求</Button>
+            </>
+          )}
         </div>
-        <p className="text-lg font-sans font-semibold text-blue-grey-800">
-          匹配结果
-        </p>
-        <div className="flex-auto w-full flex flex-wrap content-start  md:overflow-auto nm-flat-blue-grey-100 p-2 rounded-md">
-          {piece &&
-            pieces
-              .filter((p) => comparePiece(p, piece))
+        <Title>匹配结果</Title>
+        <div className="flex-auto w-full flex flex-wrap content-start md:overflow-auto nm-flat-blue-grey-100 p-2 rounded-md gap-3">
+          {matchPiece &&
+            requirePieces
+              .filter((p) => comparePiece(p, matchPiece))
               .map((p, index) => (
                 <PieceCard key={p + index} piece={p} onRemove={removePiece} />
               ))}
